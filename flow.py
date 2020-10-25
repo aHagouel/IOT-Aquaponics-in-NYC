0beee35ec7896fe549be460a72e743740464f852
ah265@duke.edu <ah265@duke.edu>
2020-10-25 16:57:24 -0400

    hooked up flow meter to use callbacks instead of pin value aafter local testing

--- flow.py
+++ flow.py
@@ -1,17 +1,21 @@
 # Goal is to turn a pump on for 30 seconds every 30 minutes.
-
 import requests
 import time
-import RPi.gpio as GPIO
+import RPi.GPIO as GPIO
+
+global pulses
+pulses = 0
 
 FLOW_SENSOR = 17
 GPIO.setmode(GPIO.BCM)
-GPIO.setup(FLOW_SENSOR, GPIO.IN)
+GPIO.setup(FLOW_SENSOR, GPIO.IN, pull_up_down = GPIO.PUD_UP)
+
 
 def turn_pump_off():
     requests.post('https://maker.ifttt.com/trigger/turn_pump_off/with/key/SECRETS_HERE',
                   params={"value1": "none", "value2": "none", "value3": "none"})
     print("Turned pump off at "+str(time.time()))
+
 
 def turn_pump_on():
     requests.post('https://maker.ifttt.com/trigger/turn_pump_on/with/key/SECRETS_HERE',
@@ -19,32 +23,48 @@ def turn_pump_on():
     print("Turned pump on at "+str(time.time()))
 
 
+# Callback when gpio pin changes value, signifying flow meter pulse. Keep simple right now to a count, but
+# in the future can actually count up the amount of water that's passed through the system.
+def pulseCallback(channel):
+    global pulses
+    pulses += 1
+    print("Detected flowing water at " + str(time.time()))
+
+
+GPIO.add_event_detect(FLOW_SENSOR, GPIO.FALLING, callback=pulseCallback)
+
+print("Starting control program at " + str(time.time()))
+turned_on = False
 while True:
-    # Make sure everything is off
-    turn_pump_off()
+    # Make sure everything is off.
+    # IFTTT takes some time to respond, so adding a buffer after every call.
+    if turned_on:
+        turn_pump_off()
+        time.sleep(5)
+        turned_on = False
 
-    #IFTTT takes a second to respond.
-    time.sleep(5)
+    global pulses
+    localPulses = pulses
+    stop = time.time() + 60*5
 
-    turned_on = False
-    stop = time.time() + 30
-    while time.time() < stop:
+    # Turn pump on for duration OR until grow bed starts draining
+    while time.time() < stop or localPulses < pulses:
 
         # Don't know how to read plug status just yet, so will assume
         # it all works until I actually code up some error handling :)
         if not turned_on:
             turn_pump_on()
+            time.sleep(5)
             turned_on = True
-            time.sleep(5)
-            continue
-        # TODO: Tests this works when you get a monitor for Pi
-        # Check if overflow initiated from grow bed, turn pump off to drain
-        if GPIO.input(FLOW_SENSOR) != 0:
-            turn_pump_off()
-            time.sleep(5)
             continue
 
         time.sleep(5)
 
-    turn_pump_off()
+    # Either time ran out or drain began, so turn off pump if it's not already
+    print("Initiating drain sequence at " + str(time.time()))
+    if turned_on:
+        turn_pump_off()
+        time.sleep(5)
+        turned_on = False
+
     time.sleep(60*30)
