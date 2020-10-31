11c5b6a252ab95329a1565bb0ced55679189f755
aHagouel <anthony.hagouel@gmail.com>
2020-10-31 18:12:39 -0400

    Added a tiny bit of exception handling, remembered how to use OR statements, and made other tweaks

--- flow.py
+++ flow.py
@@ -3,10 +3,11 @@ import time
 from datetime import datetime
 import RPi.GPIO as GPIO
 
-# Goal is to turn a pump on for 3 minutes or until ebb & flow aquaponic system floods, every 30 minutes
+# Goal is to turn a pump on for 1.5 minutes or until ebb & flow aquaponic system floods, every 30 minutes
 
-global pulses
+global pulses, turned_on
 pulses = 0
+turned_on = False
 
 FLOW_SENSOR = 17
 GPIO.setmode(GPIO.BCM)
@@ -14,45 +15,58 @@ GPIO.setup(FLOW_SENSOR, GPIO.IN, pull_up_down = GPIO.PUD_UP)
 
 
 def turn_pump_off():
-    requests.post('https://maker.ifttt.com/trigger/turn_pump_off/with/key/SECRETS_HERE',
+    global turned_on
+    try:
+        requests.post('https://maker.ifttt.com/trigger/turn_pump_off/with/key/SECRETS_HERE',
                   params={"value1": "none", "value2": "none", "value3": "none"})
-    print("Turned pump off at "+str(datetime.fromtimestamp(time.time())))
+        turned_on = False
+        print("Turned pump off at "+str(datetime.fromtimestamp(time.time())))
 
+    except:
+        #Keep trying to turn pump off! #NoFloodsPlz
+        print(">>>>> Exception found, trying to turn pump off at "+str(datetime.fromtimestamp(time.time())))
+        turn_pump_off()
+        
 
 def turn_pump_on():
-    requests.post('https://maker.ifttt.com/trigger/turn_pump_on/with/key/SECRETS_HERE',
+    global turned_on
+    try:
+        requests.post('https://maker.ifttt.com/trigger/turn_pump_on/with/key/SECRETS_HERE',
                   params={"value1": "none", "value2": "none", "value3": "none"})
-    print("Turned pump on at "+str(datetime.fromtimestamp(time.time())))
-
+        turned_on = True
+        print("Turned pump on at "+ str(datetime.fromtimestamp(time.time())))
+        
+    except:
+        turn_pump_off()
+        print(">>>>> Exception found, trying to turn pump off at "+str(datetime.fromtimestamp(time.time())))
+        
 
 # Callback when gpio pin changes value, signifying flow meter pulse. Keep simple right now to a count, but
 # in the future can actually count up the amount of water that's passed through the system.
 def pulseCallback(channel):
     global pulses
     pulses += 1
-    print("Detected flowing water at " + str(datetime.fromtimestamp(time.time())))
+    print("Detected water draining at " + str(datetime.fromtimestamp(time.time()))+ ", pulses up to " + str(pulses))
 
 
 GPIO.add_event_detect(FLOW_SENSOR, GPIO.FALLING, callback=pulseCallback)
 
 print("Starting control program at " + str(datetime.fromtimestamp(time.time())))
-turned_on = False
 while True:
     # Make sure everything is off.
     # IFTTT takes some time to respond, so adding a buffer after every call.
     if turned_on:
         turn_pump_off()
         time.sleep(5)
-        turned_on = False
+        turned_on = False  
 
-    global pulses
     localPulses = pulses
     stop = time.time() + 60*1.5
 
     # Turn pump on for flood duration (measured before coding)
     # OR until grow bed starts draining. 10 pulses subbed from global to adjust for my leaky dang drain pipe.
-    while time.time() < stop or localPulses < (pulses - 10):
-
+    while time.time() < stop and localPulses > (pulses - 10):        
+        
         # Don't know how to read plug status just yet, so will assume
         # it all works until I actually code up some error handling :)
         if not turned_on:
@@ -64,10 +78,11 @@ while True:
         time.sleep(5)
 
     # Either time ran out or drain began, so turn off pump if it's not already
-    print("Initiating drain sequence at " + str(datetime.fromtimestamp(time.time())))
     if turned_on:
+        print("Initiating drain sequence at " + str(datetime.fromtimestamp(time.time())))
         turn_pump_off()
         time.sleep(5)
         turned_on = False
 
+    print("ZzzzZZZzzzzzzZZZZZ ...Sleeping for 30 minutes... ZzzzZZZzzzzzzZZZZZ")
     time.sleep(60*30)
